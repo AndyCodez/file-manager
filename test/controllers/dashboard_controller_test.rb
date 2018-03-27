980ee87c3494827eb3c96d82ec652bd519900af0
Andrew Henry <andystudy4@gmail.com>
2018-03-27 19:53:54 +0300

    remove auto-filled controller tests

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get dashboard" do
-    get dashboard_dashboard_url
-    assert_response :success
-  end
-
 end
--- test/controllers/folders_controller_test.rb
+++ test/controllers/folders_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class FoldersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get folders_new_url
-    assert_response :success
-  end
-
 end
