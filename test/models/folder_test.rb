9b61d5746207a862838f7a1484ade67bda3d33c8
Andrew Henry <andystudy4@gmail.com>
2018-03-27 19:56:54 +0300

    test and validate folders for name presence GREEN

--- test/models/folder_test.rb
+++ test/models/folder_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class FolderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+  	@folder = Folder.new(name: "Valid folder")
+  end
+
+  test "should be valid" do
+    assert @folder.valid?
+  end
+
+  test "name must be present" do
+  	@folder.name = " "
+    assert_not @folder.valid?
+  end  
 end
