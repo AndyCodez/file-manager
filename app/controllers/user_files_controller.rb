b82e31b34c0e0232d9825e7b4101fafa2f1ed869
Andrew Henry <andystudy4@gmail.com>
2018-03-28 00:28:37 +0300

    allow for uploading of any files without restrictions

--- app/controllers/user_files_controller.rb
+++ app/controllers/user_files_controller.rb
@@ -3,4 +3,20 @@ class UserFilesController < ApplicationController
   def new
   	@user_file = UserFile.new
   end
+
+  def create
+  	@user_file = UserFile.new(user_file_params)
+  	if @user_file.save
+  		flash[:success] = "File successfully uploaded."
+  		redirect_to root_path
+  	else
+  		flash[:warning] = "Something went wrong. File not uploaded."
+  		render 'new'
+  	end
+  end
+
+  private
+  def user_file_params
+  	params.require(:user_file).permit(:file)
+  end
 end
