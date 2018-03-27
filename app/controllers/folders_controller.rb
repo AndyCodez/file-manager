29f6c0ec347ce6130a01e0ea16ad679570ca9a06
Andrew Henry <andystudy4@gmail.com>
2018-03-27 19:38:14 +0300

    redirect to the created folder

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -8,7 +8,7 @@ class FoldersController < ApplicationController
   	@folder = Folder.new(folder_params)
   	if @folder.save
   		flash[:success] = 'Folder created successfully.'
-  		redirect_to root_path
+  		redirect_to @folder
   	else
   		flash[:warning] = 'Something went wrong.'
   		render 'new'
