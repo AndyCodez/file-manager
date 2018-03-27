e829ba67b08b32ffb7fbcc043b8657f7ad59f327
Andrew Henry <andystudy4@gmail.com>
2018-03-28 00:12:13 +0300

    use resources on user_files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  
+
   root 'dashboard#dashboard'
 
   resources :folders, only: [:new, :create, :show, :destroy]
+  resources :user_files, only: [:new, :create]
 end
