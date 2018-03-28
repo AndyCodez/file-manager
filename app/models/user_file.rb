1c2c1608678c345f6beecebcaba7b71c6ce5fd69
Andrew Henry <andystudy4@gmail.com>
2018-03-28 15:33:55 +0300

    write comment

--- app/models/user_file.rb
+++ app/models/user_file.rb
@@ -1,5 +1,7 @@
 class UserFile < ApplicationRecord
   mount_uploader :file, FileUploader
   validates :file, presence: true
+  # UserFiles with nil folder_id are root files
   scope :roots, -> { where(folder_id: nil) }
+
 end
