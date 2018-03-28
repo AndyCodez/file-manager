7067011a7f670265cdb4a3d19a79d2ac08f948b7
Andrew Henry <andystudy4@gmail.com>
2018-03-28 03:25:00 +0300

    remove tests that are not needed at this point

--- test/controllers/user_files_controller_test.rb
+++ test/controllers/user_files_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class UserFilesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get user_files_new_url
-    assert_response :success
-  end
-
 end
