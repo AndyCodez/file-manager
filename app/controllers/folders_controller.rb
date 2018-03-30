b4956ddc7f1bd266a9521c537912242651d239d2
Andrew Henry <andystudy4@gmail.com>
2018-03-30 16:07:33 +0300

    redirect to folder's parent if it exists

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -8,7 +8,7 @@ class FoldersController < ApplicationController
   	@folder = Folder.new(folder_params)
   	if @folder.save
   		flash[:success] = 'Folder created successfully.'
-  		redirect_to @folder
+  		@folder.parent ? (redirect_to @folder.parent) : (redirect_to root_path)
   	else
   		render 'new'
   	end
